8e6da5338f63a5fa588e9e892c0a8d06cfb59b72
root <root@ip-10-0-0-31.ap-southeast-1.compute.internal>
2019-07-29 10:38:12 +0000

    try 4 to correct install app

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -6,16 +6,6 @@ CATALINA_HOME=/opt/tomcat
 TEMP_STAGING_DIR=/tmp/codedeploy-staging
 WAR_STAGED_LOCATION=$TEMP_STAGING_DIR/webapp/target/*.war
 
-mkdir -p $TEMP_STAGING_DIR
-sudo chown -R tomcat:tomcat $WAR_STAGED_LOCATION
-
-# Remove old application artifacts
-#if [[ -f $CATALINA_HOME/webapps/*.war ]]; then
-sudo rm -rf  $CATALINA_HOME/webapps/*.war
-#fi
-
-# Copy the WAR file to the webapps directory
-sudo ln -s $WAR_STAGED_LOCATION $CATALINA_HOME/webapps
-sudo chmod 755 $CATALINA_HOME/webapps/*.war
-sudo chown tomcat:tomcat $CATALINA_HOME/webapps/*.war
-
+ln -s $WAR_STAGED_LOCATION  $CATALINA_HOME/webapps
+chown tomcat:tomcat  $CATALINA_HOME/webapps/*.war
+chmod 755  $CATALINA_HOME/webapps/*.war
