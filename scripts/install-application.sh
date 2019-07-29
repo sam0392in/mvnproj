134b4fcc1d9dc5aac595903c2606e90ac8192498
root <root@ip-10-0-0-31.ap-southeast-1.compute.internal>
2019-07-29 11:00:15 +0000

    CICD Pipeline is working fine now. Testing phase 1

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -6,6 +6,6 @@ CATALINA_HOME=/opt/tomcat
 TEMP_STAGING_DIR=/tmp/codedeploy-staging
 WAR_STAGED_LOCATION=$TEMP_STAGING_DIR/webapp/target/*.war
 
-ln -s $WAR_STAGED_LOCATION  $CATALINA_HOME/webapps
+cp $WAR_STAGED_LOCATION  $CATALINA_HOME/webapps
 chown tomcat:tomcat  $CATALINA_HOME/webapps/*.war
 chmod 755 $CATALINA_HOME/webapps/*.war
