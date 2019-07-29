145ab1021788620e42325e827eea53e0a05af91a
root <root@ip-10-0-0-31.ap-southeast-1.compute.internal>
2019-07-29 10:39:31 +0000

    try to correct install app

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -8,4 +8,4 @@ WAR_STAGED_LOCATION=$TEMP_STAGING_DIR/webapp/target/*.war
 
 ln -s $WAR_STAGED_LOCATION  $CATALINA_HOME/webapps
 chown tomcat:tomcat  $CATALINA_HOME/webapps/*.war
-chmod 755  $CATALINA_HOME/webapps/*.war
+chmod 755 $CATALINA_HOME/webapps/*.war
