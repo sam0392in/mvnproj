a297a4bd76fd3f74982a7a75f6f146ffd82d8fc4
root <root@ip-10-0-0-31.ap-southeast-1.compute.internal>
2019-07-29 12:25:41 +0000

    testing phase 4

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -6,6 +6,8 @@ CATALINA_HOME=/opt/tomcat
 TEMP_STAGING_DIR=/tmp/codedeploy-staging
 WAR_STAGED_LOCATION=$TEMP_STAGING_DIR/webapp/target/*.war
 
+rm -rf CATALINA_HOME/webapps/*.war
+
 cp $WAR_STAGED_LOCATION  $CATALINA_HOME/webapps
 chown tomcat:tomcat  $CATALINA_HOME/webapps/*.war
 chmod 755 $CATALINA_HOME/webapps/*.war
