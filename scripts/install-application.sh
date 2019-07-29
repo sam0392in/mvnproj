4a0cf521034581ae120139ba81cb10d82f24a060
root <root@ip-10-0-0-31.ap-southeast-1.compute.internal>
2019-07-29 09:11:12 +0000

    correction in install-application.sh

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -9,12 +9,12 @@ WAR_STAGED_LOCATION=$TEMP_STAGING_DIR/webapp/target/*.war
 mkdir -p $TEMP_STAGING_DIR
 
 # Remove old application artifacts
-if [[ -f $CATALINA_HOME/webapps/*.war ]]; then
-    rm -rf  $CATALINA_HOME/webapps/*.war
-fi
+#if [[ -f $CATALINA_HOME/webapps/*.war ]]; then
+rm -rf  $CATALINA_HOME/webapps/*.war
+#fi
 
 # Copy the WAR file to the webapps directory
-cp $WAR_STAGED_LOCATION $CATALINA_HOME/webapps/
+cp /tmp/codedeploy-staging/webapp/target/*.war $CATALINA_HOME/webapps/
 chmod 755 $CATALINA_HOME/webapps/*.war
 chown tomcat:tomcat $CATALINA_HOME/webapps/*.war
 
